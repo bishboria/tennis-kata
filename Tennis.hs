c63054b6bcd84dde8ac0a565e339870c55d880f5
Stuart Gale <bishboria@gmail.com>
2012-12-03 19:43:29 +0200

    Tidying the formatting.

--- Tennis.hs
+++ Tennis.hs
@@ -24,10 +24,10 @@ data Game = Game Score Score
 instance Show Game where
     show (Game Advantage _) = "Advantage A"
     show (Game _ Advantage) = "Advantage B"
-    show (Game Deuce _) = "Deuce"
-    show (Game x y) = show x ++ " - " ++ show y
-    show WinnerA    = "A Wins"
-    show WinnerB    = "B Wins"
+    show (Game Deuce _)     = "Deuce"
+    show (Game x y)         = show x ++ " - " ++ show y
+    show WinnerA            = "A Wins"
+    show WinnerB            = "B Wins"
 
 instance Show Score where
     show Love    = "Love"
@@ -39,15 +39,15 @@ newGame :: Game
 newGame = (Game Love Love)
 
 score :: Player -> Game -> Game
-score A (Game Advantage _) = WinnerA
-score B (Game _ Advantage) = WinnerB
-score A (Game _ Advantage) = Game Deuce Deuce
-score A (Game Deuce x)  = Game Advantage x
+score A (Game Advantage _)  = WinnerA
+score B (Game _ Advantage)  = WinnerB
+score A (Game _ Advantage)  = Game Deuce Deuce
+score A (Game Deuce x)      = Game Advantage x
 score A (Game Thirty Forty) = Game Deuce Deuce
-score A (Game Forty _) = WinnerA
-score B (Game _ Forty) = WinnerB
-score A (Game x y)     = Game (succ x) y
-score B (Game x y)     = Game w z
+score A (Game Forty _)      = WinnerA
+score B (Game _ Forty)      = WinnerB
+score A (Game x y)          = Game (succ x) y
+score B (Game x y)          = Game w z
   where
     (Game z w) = score A (Game y x)
-score _ gameFinished   = gameFinished
+score _ gameFinished        = gameFinished
