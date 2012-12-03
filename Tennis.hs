734de496c5351ff82120f77d257a20e841c99c85
Stuart Gale <bishboria@gmail.com>
2012-12-03 18:59:58 +0200

    Game of tennis ignoring deuce.

---
+++ Tennis.hs
@@ -0,0 +1,43 @@
+module Tennis
+( Player(..)
+, Score(..)
+, Game(..)
+, newGame
+, score
+) where
+
+data Player = A
+            | B
+
+data Score = Love
+           | Fifteen
+           | Thirty
+           | Forty
+           deriving (Enum)
+
+data Game = Game Score Score
+          | WinnerA
+          | WinnerB
+
+instance Show Game where
+    show (Game x y) = show x ++ " - " ++ show y
+    show WinnerA    = "A Wins"
+    show WinnerB    = "B Wins"
+
+instance Show Score where
+    show Love = "Love"
+    show Fifteen = "15"
+    show Thirty = "30"
+    show Forty = "40"
+
+newGame :: Game
+newGame = (Game Love Love)
+
+score :: Player -> Game -> Game
+score A (Game Forty _) = WinnerA
+score B (Game _ Forty) = WinnerB
+score A (Game x y)     = Game (succ x) y
+score B (Game x y)     = Game w z
+  where
+    (Game z w) = score A (Game y x)
+score _ gameFinished = gameFinished
---
+++ TennisSpec.hs
@@ -0,0 +1,40 @@
+import Test.Hspec
+import Tennis
+
+scorePoints :: Int -> Player -> Game -> Game
+scorePoints 0 _ g = g
+scorePoints n p g = scorePoints (n-1) p (score p g)
+
+main :: IO()
+main = hspec $ do
+
+    describe "Game of Tennis" $ do
+
+        context "ignoring Deuce" $ do
+
+            it "starts Love each" $ do
+                (show newGame) `shouldBe` "Love - Love"
+
+            it "player A scores a point" $ do
+                (show . score A) newGame `shouldBe` "15 - Love"
+
+            it "player A scores 2 points" $ do
+                (show . scorePoints 2 A) newGame `shouldBe` "30 - Love"
+
+            it "player A scores 3 points" $ do
+                (show . scorePoints 3 A) newGame `shouldBe` "40 - Love"
+
+            it "player A scores 4 points" $ do
+                (show . scorePoints 4 A) newGame `shouldBe` "A Wins"
+
+            it "player B scores a point" $ do
+                (show . score B) newGame `shouldBe` "Love - 15"
+
+            it "player B scores 2 points" $ do
+                (show . scorePoints 2 B) newGame `shouldBe` "Love - 30"
+
+            it "player B scores 3 points" $ do
+                (show . scorePoints 3 B) newGame `shouldBe` "Love - 40"
+
+            it "player B scores 4 points" $ do
+                (show . scorePoints 4 B) newGame `shouldBe` "B Wins"
